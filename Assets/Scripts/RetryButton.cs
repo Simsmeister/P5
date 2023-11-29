fbe0e5a32701374ae215df60d029a175a5ad9817
Zextir <gabriel.deangelis007@gmail.com>
2023-11-29 13:32:41 +0100

    push

---
+++ Assets/Scripts/RetryButton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public Button yourButton; // Reference to your button in the Unity Editor
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        // Load the "NewScene" when the button is clicked
+        SceneManager.LoadScene("NewScene");
+    }
+}
