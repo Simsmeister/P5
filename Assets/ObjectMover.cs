8991a9a5e5920a0d525109f96ae0d90b3b7ca34f
Zextir <gabriel.deangelis007@gmail.com>
2023-12-06 21:37:20 +0100

    New Stuff and firefly

---
+++ Assets/ObjectMover.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    public Transform[] waypoints;   // Assign the waypoints in the Inspector
+    public float movementSpeed = 1.0f; // Adjust the movement speed in the Inspector
+
+    private int currentWaypointIndex = 0;
+    private float journeyLength;
+    private float startTime;
+
+    void Start()
+    {
+        // Calculate the total distance between waypoints
+        CalculateJourneyLength();
+
+        // Record the start time
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        // Calculate the distance covered based on time passed and speed
+        float distanceCovered = (Time.time - startTime) * movementSpeed;
+
+        // Calculate the fraction of the journey completed
+        float fractionOfJourney = distanceCovered / journeyLength;
+
+        // Move the object using Lerp (Linear Interpolation)
+        transform.position = Vector3.Lerp(waypoints[currentWaypointIndex].position, waypoints[currentWaypointIndex + 1].position, fractionOfJourney);
+
+        // Check if the object has reached the current waypoint
+        if (fractionOfJourney >= 1.0f)
+        {
+            // Move to the next waypoint
+            currentWaypointIndex++;
+
+            // Check if all waypoints have been visited
+            if (currentWaypointIndex < waypoints.Length - 1)
+            {
+                // Calculate the total distance between the new waypoints
+                CalculateJourneyLength();
+
+                // Record the start time for the new journey
+                startTime = Time.time;
+            }
+            else
+            {
+                // Optionally, you can perform additional actions when all waypoints are visited
+                // For example, reset the movement to loop through waypoints again
+                // currentWaypointIndex = 0;
+                // startTime = Time.time;
+            }
+        }
+    }
+
+    void CalculateJourneyLength()
+    {
+        journeyLength = Vector3.Distance(waypoints[currentWaypointIndex].position, waypoints[currentWaypointIndex + 1].position);
+    }
+}
