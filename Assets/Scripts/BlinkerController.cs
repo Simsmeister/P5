f8c707935d802ded9eafb41a54551ea321f0cfcc
Zextir <gabriel.deangelis007@gmail.com>
2023-12-12 20:19:11 +0100

    Scenario #3 næsten done

--- Assets/Scripts/BlinkerController.cs
+++ Assets/Scripts/BlinkerController.cs
@@ -11,6 +11,7 @@ public class BlinkerController : MonoBehaviour
     private Renderer ren;
     private AudioSource audioSource;
     private bool insideBlinkerBox = false;
+    private float targetVolume = 0.2f; // Default volume level
 
     private void Awake()
     {
@@ -19,6 +20,7 @@ public class BlinkerController : MonoBehaviour
         audioSource.clip = blinkerSound;
         audioSource.loop = true; // Adjust as needed
         audioSource.playOnAwake = false;
+        audioSource.volume = targetVolume; // Set initial volume
     }
 
     private void Update()
@@ -26,6 +28,7 @@ public class BlinkerController : MonoBehaviour
         if (insideBlinkerBox)
         {
             ren.material.color = Color.Lerp(startColor, endColor, Mathf.PingPong(Time.time * colorChangeSpeed, 1));
+
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -35,6 +38,15 @@ public class BlinkerController : MonoBehaviour
         {
             audioSource.Stop();
         }
+
+        // Adjust volume dynamically
+        audioSource.volume = Mathf.Lerp(audioSource.volume, targetVolume, Time.deltaTime * 5.0f);
+    }
+
+    // Function to set the target volume
+    public void SetVolume(float volume)
+    {
+        targetVolume = Mathf.Clamp01(volume);
     }
 
     private void OnTriggerEnter(Collider other)
